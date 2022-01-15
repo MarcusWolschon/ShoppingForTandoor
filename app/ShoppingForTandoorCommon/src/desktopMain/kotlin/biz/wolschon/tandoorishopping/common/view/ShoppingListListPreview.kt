d2bcd705f20b7410890125f7a06f28cc1bf89b2f
Marcus Wolschon <Marcus@Wolschon.biz>
2022-01-15 22:27:16 +0100

    renaming

---
+++ app/ShoppingForTandoorCommon/src/desktopMain/kotlin/biz/wolschon/tandoorishopping/common/view/ShoppingListListPreview.kt
@@ -0,0 +1,34 @@
+package biz.wolschon.tandoorshopping.common.view
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import biz.wolschon.tandoorshopping.common.api.model.TandoorShoppingList
+
+@Preview
+@Composable
+fun shoppingListListPreview() {
+    val shoppingLists = listOf(
+        TandoorShoppingList(
+            id = 0,
+            uuid = "000",
+            note = "get for sunday",
+            entries = setOf(),
+            finished = false
+        ),
+        TandoorShoppingList(
+            id = 1,
+            uuid = "111",
+            note = "get from market",
+            entries = setOf(),
+            finished = false
+        ),
+        TandoorShoppingList(
+            id = 2,
+            uuid = "222",
+            note = "alredy got these",
+            entries = setOf(),
+            finished = true
+        )
+    )
+    shoppingListList(shoppingLists, showFinished = true) {}
+}
