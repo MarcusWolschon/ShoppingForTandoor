502cb3307f6c1368cbb2ad9aacba8a98ae484f53
Marcus Wolschon <Marcus@Wolschon.biz>
2022-02-03 16:35:38 +0100

    more memory for gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath("com.squareup.sqldelight:gradle-plugin:$sqdelightVersion")
     }
 }
