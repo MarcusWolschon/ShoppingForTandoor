585c879c95f4c92c0cd5ccab8627b16044c97613
Marcus Wolschon <Marcus@Wolschon.biz>
2022-02-05 08:56:11 +0100

    fix desktop build

--- app/ShoppingForTandoorCommon/src/desktopMain/kotlin/biz/wolschon/tandoorshopping/common/icons.kt
+++ app/ShoppingForTandoorCommon/src/desktopMain/kotlin/biz/wolschon/tandoorshopping/common/icons.kt
@@ -3,10 +3,10 @@ package biz.wolschon.tandoorshopping.common
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 
-actual val icon_list = @Composable { painterResource("ic_basket") }
-actual val icon_shop = @Composable { painterResource("ic_shopping_cart") }
-actual val icon_food = @Composable { painterResource("ic_food_apple") }
-actual val icon_settings = @Composable { painterResource("ic_settings") }
-actual val icon_web = @Composable { painterResource("ic_web") }
-actual val icon_menu = @Composable { painterResource("ic_menu") }
-actual val icon_refresh = @Composable { painterResource("ic_refresh") }
+actual val icon_list = @Composable { painterResource("ic_basket.xml") }
+actual val icon_shop = @Composable { painterResource("ic_shopping_cart.xml") }
+actual val icon_food = @Composable { painterResource("ic_food_apple.xml") }
+actual val icon_settings = @Composable { painterResource("ic_settings.xml") }
+actual val icon_web = @Composable { painterResource("ic_web.xml") }
+actual val icon_menu = @Composable { painterResource("ic_menu.xml") }
+actual val icon_refresh = @Composable { painterResource("ic_refresh.xml") }
